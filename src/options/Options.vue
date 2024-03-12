f288ab702bcd503d41c93d86161d9d05d00b37a6
杨国微(10031682) <yanggw@glodon.com>
2024-03-12 15:12:54 +0800

    feat: option view done

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
+import { IconEmail, IconUser } from '@arco-design/web-vue/es/icon'
 import { blockedUrls } from '~/logic'
-import { addBlockURL } from '~/logic/general'
-
-// import { Tooltip } from '@arco-design/web-vue';
+import { addBlockURL, removeBlockURL } from '~/logic/general'
 
 const form = reactive({
   newUrl: '',
@@ -27,13 +26,15 @@ const addUrl = ({ values, errors }: { values: typeof form;errors: any[] }) => {
   // console.log('values:', values, '\nerrors:', errors)
 }
 
-const updateUrl = (record) => {
+// const updateUrl = (record: any) => {
+//   console.log('updateUrl', record)
+// }
 
+const removeUrl = (record: { url: string }) => {
+  console.log('remove', record)
+  removeBlockURL(record.url)
 }
-
-const removeUrl = (record) => {
-
-}
+// const urls = computed(() => blockedUrls.value.sort((a, b) => (a > b) ? 1 : -1).map(url => ({ url })))
 const urls = computed(() => blockedUrls.value.map(url => ({ url })))
 </script>
 
@@ -65,21 +66,35 @@ const urls = computed(() => blockedUrls.value.map(url => ({ url })))
       </a-form>
       <a-divider />
       {{ blockedUrls }}
-      <a-table :columns="columns" :data="urls" :pagination="{ 'show-page-size': true, 'default-page-size': 6, 'show-total': true }">
+      <a-table :columns="columns" :data="urls" :pagination="{ 'show-page-size': true, 'show-total': true }">
         <template #actions="{ record }">
           <a-space>
-            <a-button @click="updateUrl(record)">
+            <!-- <a-button @click="updateUrl(record)">
               Edit
-            </a-button>
-            <a-button type="primary" status="danger" @click="removeUrl(record)">
-              Delete
-            </a-button>
+            </a-button> -->
+            <a-popconfirm :content="`Delete '${record.url}' ?`" ok-text="OK" cancel-text="Cancel" @ok="removeUrl(record)">
+              <a-button type="primary" status="danger">
+                Delete
+              </a-button>
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
     </a-layout-content>
     <a-layout-footer class="h-12 flex items-center justify-center">
-      URL BLock
+      <a-space size="large">
+        <div class="text-gray-500 font-xl">
+          <span>URL BLock</span>
+        </div>
+        <div class="text-gray-500">
+          <IconUser class="mr-1" />
+          <span>Author: Eric Yang</span>
+        </div>
+        <div class="text-gray-500">
+          <IconEmail class="mr-1" />
+          <span>Mail: ygweric@gmail.com</span>
+        </div>
+      </a-space>
     </a-layout-footer>
   </a-layout>
 </template>
