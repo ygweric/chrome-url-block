5e9ec8c5796117e93975053cf94a5a90538bf0a8
杨国微(10031682) <yanggw@glodon.com>
2024-03-14 18:36:15 +0800

    feat: 优化content内样式

--- src/styles/contentStyles.ts
+++ src/styles/contentStyles.ts
@@ -1,3 +1,3 @@
 // 这里不能引入总styles，因为会和宿主页面冲突，因此只引入tailwindCss和Arco
-import '@arco-design/web-vue/dist/arco.css'
+// import '@arco-design/web-vue/dist/arco.css'
 import './contentStyles.css'
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,2 @@
 import './main.css'
-import './contentStyles.css'
 import '@arco-design/web-vue/dist/arco.css'
